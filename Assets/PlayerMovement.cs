39a93899ded2b2289cdc99d334d3f329e982b2de
Alfadas <alfadas@mail.goodevening-copr.de>
2018-04-21 19:56:31 +0200

    Added Player Placeholder, Basic left/right Player Controlls, Rigidbodys

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float Speed = 10f;
+    [SerializeField] float xRange = 7.4f;
+
+    float horizontalThrow;
+    bool movementEnabled;
+
+
+    void Update ()
+    {
+    if (movementEnabled == true)
+        {
+            horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        }
+
+    HorizontalMovement();
+            
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enviroment")
+        {
+            movementEnabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enviroment")
+        {
+            movementEnabled = false;
+        }
+    }
+
+
+    void HorizontalMovement()
+    {
+        
+        float xOffset = horizontalThrow * Speed * Time.deltaTime;
+        float NewXPos = transform.localPosition.x + xOffset;
+        transform.localPosition = new Vector3(NewXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
