7a2255fe95992dd7505e289f258b7f6a33e511ea
Alfadas <alfadas@mail.goodevening-copr.de>
2018-04-22 21:01:20 +0200

    added Tower Spawn

---
+++ Assets/Scripts/Player/BuildingManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    public GameObject[] Towers;
+    TowerPlacement towerPlacement;
+
+	void Start ()
+    {
+        towerPlacement = GetComponent<TowerPlacement>();
+	}
+	void Update ()
+    {
+		
+	}
+    void OnGUI()
+    {
+        for (int i = 0; i < Towers.Length; i++)
+        {
+            if (GUI.Button(new Rect(Screen.width / 20, Screen.height / 15 + Screen.height / 20 * i, 170, 30), Towers[i].name))
+            {
+                towerPlacement.SetItem(Towers[i]);
+            }
+        }
+    }
+}
