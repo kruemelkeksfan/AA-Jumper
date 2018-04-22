a485bdb014f9c01dfaaf1c60b81acc5443d11e8a
Alfadas <alfadas@mail.goodevening-copr.de>
2018-04-22 05:26:54 +0200

    changed Prefab so that ervery Dreadnought can drop bombs, surviving Dreadnoughts reset to spawn, Script Reorganization

--- Assets/Scripts/Enemie/BombSpawner.cs
+++ Assets/Scripts/Enemie/BombSpawner.cs
@@ -8,15 +8,20 @@ public class BombSpawner : MonoBehaviour
     [SerializeField] float bombDropLocation = 4f;
     [SerializeField] GameObject Bomb;
 
-    bool bombeDropped = false;
+    bool bombeDropped;
 
 
     void Update ()
     {
+        if (gameObject.transform.position.x >= 60)
+        {
+            bombeDropped = false;
+        }
+
         if (bombeDropped == false && gameObject.transform.position.x <= bombDropLocation)
         {
             bombeDropped = true;
             Instantiate (Bomb, gameObject.transform.position, Quaternion.identity);
         }
-	}
+    }
 }
---
+++ Assets/Scripts/Enemie/EnemieController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieController : MonoBehaviour
+{
+
+    [SerializeField] float movementSpeed;
+    [Tooltip ("in sec")][SerializeField] int restartTime;
+
+    private Rigidbody rigidBody;
+   
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+        rigidBody.velocity = -transform.right * movementSpeed;
+    }
+
+    void Update()
+    {
+        if (gameObject.transform.position.x <= -5)
+        {
+            Invoke("Restart", restartTime);
+        }
+    }
+
+    void Restart()
+    {
+        gameObject.transform.position = new Vector3 (65, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+}
