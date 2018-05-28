e79b67e0ce76fac32d446a2cda32a85f144f52b7
Alfadas <alfadas@mail.goodevening-copr.de>
2018-05-28 14:19:14 +0200

    toggle group for difficulty settings in Main Menu, some Main Menu font size adjustments

---
+++ Assets/DifficultySet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultySet : MonoBehaviour {
+
+    public bool easy;
+    public static bool normal;
+    public static bool hard;
+    public static bool dreadnought;
+
+    [SerializeField] Toggle easyT;
+    [SerializeField] Toggle normalT;
+    [SerializeField] Toggle hardT;
+    [SerializeField] Toggle dreadnoughtT;
+
+
+    void Start ()
+    {
+        easy = false;
+        normal = true;
+        hard = false;
+        dreadnought = false;
+       
+    }
+    private void Update()
+    {
+        if (easyT.isOn)
+        {
+            easy = true;
+            normal = false;
+            hard = false;
+            dreadnought = false;
+        }
+        else if (normalT.isOn)
+        {
+            easy = false;
+            normal = true;
+            hard = false;
+            dreadnought = false;
+        }
+        else if (hardT.isOn)
+        {
+            easy = false;
+            normal = false;
+            hard = true;
+            dreadnought = false;
+        }
+        else if (dreadnoughtT.isOn)
+        {
+            easy = false;
+            normal = false;
+            hard = false;
+            dreadnought = true;
+        }
+    }
+}
