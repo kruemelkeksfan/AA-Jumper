0bc559fcd67f1e0311ec1edba1e845fdc0a2b4cf
Alfadas <alfadas@mail.goodevening-copr.de>
2018-06-22 17:02:54 +0200

    added credits

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] GameObject gameName;
+    [SerializeField] GameObject credits;
+
+    bool creditsactive;
+
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1))
+        {
+            creditsactive = false;
+            gameName.SetActive(true);
+            credits.SetActive(false);
+        }
+	}
+    public void ToggleCredits()
+    {
+        creditsactive = !creditsactive;
+        gameName.SetActive(!creditsactive);
+        credits.SetActive(creditsactive);
+    }
+}
